15b1c18ab5656cd0d26874400dbbaecb6eea41c3
Anushree602 <anushreemoraskar86@gmail.com>
2026-09-04 16:24:21 +0530

    day-2 files added

---
+++ day-2/var.tf
@@ -0,0 +1,39 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_cidr" {
+    default = "10.0.0.0/20"
+}
+
+variable "public_az" {
+    default = "ap-south-1a"
+}
+
+variable "private_cidr" {
+    default = "10.0.16.0/20"
+}
+
+variable "private_az" {
+    default = "ap-south-1b"
+}
+
+variable "ami" {
+    default = "ami-01a00762f46d584a1"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_name" {
+    default = "med-erp-key"
+}
+
+variable "volume_size" {
+    default = 10
+}
+
+variable "volume_type" {
+    default = "gp3"
+}
