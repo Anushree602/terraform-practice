503a90163a9be78e7bb93e04b8d63b0256c95d0a
Anushree602 <anushreemoraskar86@gmail.com>
2026-09-08 21:33:30 +0530

    Update security group ID for public_instance in main.tf

--- day-5/main.tf
+++ day-5/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public_instance" {
    instance_type = "t3.micro"
    key_name      = "med-erp-key"
    count        = 2
-   vpc_security_group_ids = ["sg-0e49c59ffa4c69279"]
+   vpc_security_group_ids = ["sg-0e7fe9ac18d6d1d74"]
    tags = {
      Name = "public_instance"
    }
