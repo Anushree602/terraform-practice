31765204b172a875535b38efedf2360153db99ad
Anushree602 <anushreemoraskar86@gmail.com>
2026-09-08 16:15:35 +0530

    Update AMI ID for public_instance in main.tf

--- day-5/main.tf
+++ day-5/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "public_instance" {
-   ami           = "ami-0c55b159cbfafe1f0"
+   ami           = "ami-01a00762f46d584a1"
    instance_type = "t3.micro"
    key_name      = "med-erp-key"
    count        = 2
