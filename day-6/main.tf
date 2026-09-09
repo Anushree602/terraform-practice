8f3d710c106b23610744a9e935b6f9c2610492e1
Anushree602 <anushreemoraskar86@gmail.com>
2026-09-09 16:42:40 +0530

    DAY

--- day-6/main.tf
+++ day-6/main.tf
@@ -4,7 +4,6 @@ resource "random_id" "random_id" {
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "my-bucket-${terraform.workspace}-${random_id.random_id.hex}"
-  acl    = "private"
     tags = {
         Name        = "my-bucket-${terraform.workspace}"
        
