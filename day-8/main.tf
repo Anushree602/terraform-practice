9d7b334c04f85fa4f7df73ce9e46d3ec183ee780
Anushree602 <anushreemoraskar86@gmail.com>
2026-09-10 13:40:16 +0530

    Add AWS EC2 instance configuration with file provisioner and SSH connection in main.tf; include private key in med-erp-key.pem; create provider.tf

---
+++ day-8/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "example" {
+  instance_type          = "t3.micro"
+  ami                    = "ami-01a00762f46d584a1"
+  key_name               = "med-erp-key"
+  count                  = 2
+  vpc_security_group_ids = ["sg-0e7fe9ac18d6d1d74"]
+
+  provisioner "file" {
+    source      = "hello.txt"
+    destination = "/home/ubuntu/hello.txt"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/med-erp-key.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd"
+    ]
+  }
+}
